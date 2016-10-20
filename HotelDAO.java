fdd8b6d9884b163d9ce3ab8f1e67e05a9aa92a1b
victoriatanaka <victoriatanaka@hotmail.com>
2016-10-20 16:15:52 -0200

    Add files via upload

---
+++ HotelDAO.java
@@ -0,0 +1,11 @@
+package br.com.sistemadevendas.dao;
+import java.util.List;
+import br.com.sistemadevendas.models.Hotel;
+
+public interface HotelDAO {
+   public List<Hotel> getHoteis();
+   public Hotel getHotel(int ID);
+   public void adicionarHotel(Hotel hotel);
+   public void alterarHotel(Hotel hotel);
+   public void deletarHotel(Hotel hotel);
+}
